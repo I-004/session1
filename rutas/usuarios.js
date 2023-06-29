54ac0f73fa4c80d9847f4db5fe2adf85da3fd1ee
I-004 <ivanlp.ti22@utsjr.edu.mx>
2023-06-29 09:04:27 -0600

    first comit

---
+++ rutas/usuarios.js
@@ -0,0 +1,20 @@
+var ruta=require("express").Router();
+
+ruta.get("/",(req, res)=>{
+    res.render("Inicio");
+});
+
+ruta.get("/login",(req, res)=>{
+    res.render("login");
+});
+
+ruta.post("/revisar",(req, res)=>{
+    if(req.body.usuario=="abc" && req.body.password=="123"){
+        res.render("bienvenido");
+    }
+    else{
+        res.render("error");
+    }
+});
+
+module.exports=ruta;
